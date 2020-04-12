d76a328a869af54872db53906812990d21a9fa0d
Richard Whitehead <damian253@me.com>
2020-04-11 17:05:26 -0700

    updated location

--- server.js
+++ server.js
@@ -7,41 +7,48 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
+const superagent = require('superagent');
 
 const PORT = process.env.PORT;
 
 const app = express();
 app.use(cors());
 
-// Test Endpoint
-// http://localhost:3000/test
-app.get( '/test', (request, response) => {
-  const name = request.query.name;
-  response.send( `Hello, ${name}` );
-});
-
-app.get('/cats', (request, response) => {
-  let type = request.query.type;
-  let words = '';
-  if ( type === 'calico' ) {
-    words = 'You are a good person';
-  }
-  else {
-    words = 'We do not have those';
-  }
-
-  response.send(words);
-});
 
 app.get('/location', handleLocation);
 
 function handleLocation( request, response ) {
-  let city = request.query.city;
-  // eventually, get this from a real live API
-  // But today, pull it from a file.
-  let locationData = require('./data/geo.json');
-  let location = new Location(city, locationData[0]);
-  response.json(location);
+  try {
+    let city = request.query.city;
+    // eventually, get this from a real live API
+    // But today, pull it from a file.
+
+    // throw 'john is ugly or something';
+
+    const url = 'https://us1.locationiq.com/v1/search.php';
+    const queryStringParams = {
+      key: process.env.LOCATION_TOKEN,
+      q: city,
+      format: 'json',
+      limit: 1,
+    };
+
+    superagent.get(url)
+      .query(queryStringParams)
+      .then( data => {
+        let locationData = data.body[0];
+        console.log(locationData);
+        let location = new Location(city, locationData);
+        response.json(location);
+      });
+  }
+  catch(error) {
+    let errorObject = {
+      status: 500,
+      responseText: 'try again',
+    };
+    response.status(500).json(errorObject);
+  }
 }
 
 function Location(city, data) {
@@ -51,36 +58,54 @@ function Location(city, data) {
   this.longitude = data.lon;
 }
 
-/* Restaurants
-  {
-    "restaurant": "Serious Pie",
-    "cuisines": "Pizza, Italian",
-    "locality": "Belltown"
-  },
-*/
 
-app.get('/restaurants', handleRestaurants);
+app.get('/weather', handleWeather);
 
-function handleRestaurants(request, response) {
-  // Eventually, will be an API call
-  // Today ... get a file
+function handleWeather(request, response) {
+  try {
+  // use darksky fake data
+  // eventually will be an api call
+  // let weatherData = require('./data/darksky.json');
+    let listofDays = [];
+    let url = 'https://api.darksky.net/forecast';
+    let lat = request.query.latitude;
+    let lon = request.query.longitude;
 
-  let restaurantData = require('./data/restaurants.json');
-  let listOfRestaurants = [];
+    // user-key
 
-  restaurantData.nearby_restaurants.forEach( r => {
-    let restaurant = new Restaurant(r);
-    listOfRestaurants.push(restaurant);
-  });
+    superagent.get(url)
+      .set('user-key', process.env.DARKSKY_TOKEN)
+      .set('latitude', lat)
+      .set('longitude', lon)
+      .then( data => {
+        data.daily.data.map( day => {
+          let weather = new Weather(day);
+          listofDays.push(weather);
+        })
+        response.json(listofDays);
+      }).catch( error => console.log(error));
 
-  response.json(listOfRestaurants);
+    // weatherData.daily.data.map( day => {
+    //   let weather = new Weather(day);
+    //   listofDays.push(weather);
+    // })
 
+  }
+
+  catch(error) {
+    let errorObject = {
+      status: 500,
+      responseText: 'john is ugly or something',
+    };
+    response.status(500).json(errorObject);
+  }
 }
 
-function Restaurant(data) {
-  this.name = data.restaurant.name;
-  this.cuisines = data.restaurant.cuisines;
-  this.locality = data.restaurant.location.locality;
+function Weather(data) {
+  this.time = data.time;
+  this.forecast = data.summary;
 }
+
+
 
 app.listen( PORT, () => console.log('Server up on', PORT));
