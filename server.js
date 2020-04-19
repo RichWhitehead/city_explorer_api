8e0997fc1c583e9932581e83e41bfbb9e34af0fc
Richard Whitehead <damian253@me.com>
2020-04-18 17:46:28 -0700

    lab08 complete

--- server.js
+++ server.js
@@ -5,53 +5,73 @@
   PORT=3000
 */
 require('dotenv').config();
+
+// globals
+
 const cors = require('cors');
 const express = require('express');
 const superagent = require('superagent');
 const PORT = process.env.PORT;
 const app = express();
+const pg = require('pg');
 app.use(cors());
 
 // connect to database
-// const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', (error) => console.error('Connection Failure', error));
 
-//empty cache for location
-// let locationCache ={};
+//empty cache for locationnodemon
 
 
 app.get('/location', handleLocation); //Handler function for location
 
 function handleLocation( request, response ) {
-  try {
-    let city = request.query.city;
-    // eventually, get this from a real live API
-    // But today, pull it from a file.
 
-    const url = 'https://us1.locationiq.com/v1/search.php';
-    const queryStringParams = {
-      key: process.env.LOCATION_TOKEN,
-      q: city,
-      format: 'json',
-      limit: 1,
-    };
 
-    superagent.get(url)
-      .query(queryStringParams)
-      .then( data => {
-        let locationData = data.body[0];
-        // console.log(locationData);
-        let location = new Location(city, locationData);
-        response.json(location);
-      });
-  }
-  catch(error) {
-    let errorObject = {
-      status: 500,
-      responseText: 'john is ugly or something',
-    };
-    response.status(500).json(errorObject);
-  }
+  let city = request.query.city;
+  const url = 'https://us1.locationiq.com/v1/search.php';
+  const queryStringParams = {
+    key: process.env.LOCATION_TOKEN,
+    q: city,
+    format: 'json',
+    limit: 1,
+  };
+
+  const searchSQL = `SELECT * FROM locations WHERE search_query = $1`;
+  const values = [city];
+  client.query(searchSQL, values)
+    .then( results => {
+      console.log(results);
+      if (results.rowCount >= 1 ){
+        console.log('Response came from Database - Row count = ' + results.rowCount);
+        response.json(results.rows[0]);
+
+      } else {
+        console.log("test");
+        superagent.get(url)
+          .query(queryStringParams)
+          .then( data => {
+            let locationData = data.body[0];
+            console.log(locationData);
+            let location = new Location(city, locationData);
+            console.log(city + ' came from API');
+            const addSQL = `INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES($1, $2, $3, $4)`;
+            response.json(location)
+            ;
+            let VALUES = [location.search_query, location.formatted_query, location.latitude, location.longitude];
+            client.query(addSQL, VALUES)
+              .then(result => {
+                console.log(VALUES);
+                response.json(location);
+              });
+          });
+      }
+    })
+    .catch(e => console.log (e));
+
 }
+
 
 function Location(city, data) {
   this.search_query = city;
